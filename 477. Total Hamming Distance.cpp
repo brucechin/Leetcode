c1238c8b4f0f25594e37ac1f39dabe4b6f2515d6
Lianke <1026193951@sjtu.edu.cn>
2017-11-24 17:27:25 +0800

    11-24

---
+++ 477. Total Hamming Distance.cpp
@@ -0,0 +1,38 @@
+/*
+
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Now your job is to find the total Hamming distance between all pairs of the given numbers.
+
+Example:
+Input: 4, 14, 2
+
+Output: 6
+
+Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
+showing the four bits relevant in this case). So the answer will be:
+HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+Note:
+Elements of the given array are in the range of 0 to 10^9
+Length of the array will not exceed 10^4.
+
+*/
+
+/*
+
+每次对所有数最低位计算1和0的个数，相乘即为当前bit的bitcount
+
+*/
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int total = 0, n = nums.size();
+        for (int j=0;j<32;j++) {
+            int bitCount = 0;
+            for (int i=0;i<n;i++) 
+                bitCount += (nums[i] >> j) & 1;
+            total += bitCount*(n - bitCount);
+        }
+        return total;
+    }
+};
